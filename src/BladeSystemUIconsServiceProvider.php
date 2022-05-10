a71438105373758ca488d5d1b362994f965c2855
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-05-10 07:09:33 +0000

    Fix styling

--- src/BladeSystemUIconsServiceProvider.php
+++ src/BladeSystemUIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeSystemUIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-system-uicons', []);
 
-            $factory->add('system-uicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('system-uicons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-system-uicons.php', 'blade-system-uicons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-system-uicons.php', 'blade-system-uicons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-system-uicons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-system-uicons'),
             ], 'blade-sui'); // TODO: update the alias to `blade-system-uicons` in next major release
 
             $this->publishes([
-                __DIR__.'/../config/blade-system-uicons.php' => $this->app->configPath('blade-system-uicons.php'),
+                __DIR__ . '/../config/blade-system-uicons.php' => $this->app->configPath('blade-system-uicons.php'),
             ], 'blade-system-uicons-config');
         }
     }
